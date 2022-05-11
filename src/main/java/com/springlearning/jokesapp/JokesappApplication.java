905b42bc124d33ef0f3f2967a5d51ccec49eab93
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-11 09:28:56 +0200

    Add Thymeleaf view

--- src/main/java/com/springlearning/jokesapp/JokesappApplication.java
+++ src/main/java/com/springlearning/jokesapp/JokesappApplication.java
@@ -2,8 +2,9 @@ package com.springlearning.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class JokesappApplication {
 
 	public static void main(String[] args) {
